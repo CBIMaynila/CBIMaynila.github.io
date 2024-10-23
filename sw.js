5c4d1d73c49e816ff0639e7c77fe6cb619e7d318
ejbau <ejbautista0209@gmail.com>
2024-10-23 12:44:26 +0800

    Update service worker with error handling and correct paths

--- sw.js
+++ sw.js
@@ -39,32 +39,12 @@ self.addEventListener('activate', event => {
 async function fetchAssets(event) {
     try {
         const response = await fetch(event.request);
-        
-        // Check if the response is OK (status code 200)
-        if (!response || !response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        
-        return response; // Return the valid response
-    } catch (error) {
-        console.error('Fetch failed; returning cached response:', error);
-        
+        return response;
+    } catch (err) {
         const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(event.request);
-        
-        // Return the cached response if it exists; if not, return a default response
-        if (cachedResponse) {
-            return cachedResponse;
-        } else {
-            // Create a default response if no cached response is available
-            return new Response('Offline', {
-                status: 404,
-                statusText: 'Not Found'
-            });
-        }
+        return cache.match(event.request);
     }
 }
-
 
 self.addEventListener('fetch', event => {
     console.log("[SW] fetched");
