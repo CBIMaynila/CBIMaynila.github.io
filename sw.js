a741ad2add89d4c0844f34b8e3e1b7a72f816856
Maynila <maynilacbi@gmail.com>
2024-10-23 11:58:25 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "static_cache"
+const STATIC_ASSETS = [
+    '/index.html',
+    '/indexpaco.html',
+    '/images/Paco_background.png'
+]
+
+async function preCache() {
+    const cache = await caches.open(CACHE_NAME)
+    return cache.addAll(STATIC_ASSETS)
+}
+
+self.addEventListener('install', event => {
+    console.log("[SW] installed");
+    self.skipWaiting()
+    event.waitUntil(preCache())
+})
+
+async function cleanupCache() {
+    const keys = await caches.keys()
+    const keysToDelete = keys.map(key => {
+        if (key !== CACHE_NAME) {
+            return caches.delete(key)
+        }
+    })
+
+    return Promise.all(keysToDelete)
+}
+
+self.addEventListener('activate', event => {
+    console.log("[SW] activated");
+    event.waitUntil(cleanupCache())
+})
+
+async function fetchAssets(event) {
+    try{
+        const response = await fetch(event.request)
+        return response
+    } catch(err) {
+        const cache = await caches.open(CACHE_NAME)
+        return cache.match(event.request)
+    }
+}
+
+self.addEventListener('fetch', event => {
+    console.log("[SW] fetched");
+    event.respondWith(fetchAssets(event))
+})
